7c07e42624c1605d19ba136d0a7305c626a57d32
Evan-liu-1018 <1132192506@qq.com>
2023-09-15 09:29:37 +0800

    fix:修改检索bug

--- docs/utils/string.ts
+++ docs/utils/string.ts
@@ -3,10 +3,10 @@
  * @param text
  * @param searchStr
  */
-export function countOccurrences(text:string, searchStr:string):number {
+export function countOccurrences(text: string, searchStr: string): number {
+    if (!text || !searchStr) return 0
     const regex = new RegExp(searchStr, 'g');
     const matches = text.match(regex);
-
     if (matches) {
         return matches.length;
     } else {
